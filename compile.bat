191e19899f719e246a4a1ec45739c3bee6ba1025
Martin-Lacroix <lacroix.martin@protonmail.com>
2021-03-24 21:24:28 +0100

    MKL

--- compile.bat
+++ compile.bat
@@ -1,2 +1,6 @@
-g++ -O3 -I C:\ProgramData\Alglib\src -L C:\Users\ORBBE\Desktop\Plugin\include\ ^
-source\*.cpp C:\ProgramData\Alglib\src\*.cpp -o Mechanics.exe
+g++ -I C:\ProgramData\Alglib\src -L C:\Users\ORBBE\Desktop\Plugin\include\ ^
+source\*.cpp C:\ProgramData\Alglib\src\*.cpp -o Mechanics.exe -O3 ^
+-DAE_CPU=AE_INTEL -DAE_OS=AE_WINDOWS -pthread -DAE_MKL alglib*64mkl.lib
+
+::g++ -O3 -I C:\ProgramData\Alglib\src -L C:\Users\ORBBE\Desktop\Plugin\include\ ^
+::source\*.cpp C:\ProgramData\Alglib\src\*.cpp -o Mechanics.exe
